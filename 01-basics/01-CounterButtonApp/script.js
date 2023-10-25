a283d3d85428c36d9ac82ba66ed833c8ffad4708
velibegov <velibegov@mail.ru>
2023-10-25 19:07:33 +0300

    solve increment counter

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,9 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      count: 0,
+    }
+  }
+}).mount('#app');
