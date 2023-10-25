b842efa5d6f32003bbff77b33cc39cab617058dd
velibegov <velibegov@mail.ru>
2023-10-25 21:02:14 +0300

    solve calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,27 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      operator: undefined,
+    }
+  },
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'sum':
+          return this.a + this.b;
+        case 'subtract':
+          return this.a - this.b;
+        case 'multiply':
+          return this.a * this.b;
+        case 'divide':
+          return this.a / this.b;
+        default:
+          return 0;
+      }
+    }
+  },
+}).mount('#app');
